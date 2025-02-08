395793cf424f713ebfcd17d6e21698f22cfb67b4
Jeongbowoon <happyanne200212@gmail/com>
2025-02-09 00:06:33 +0900

    feat: mypage 초기 세팅 및 back arrow 생성

--- src/pages/myPage/MyPage.jsx
+++ src/pages/myPage/MyPage.jsx
@@ -1,5 +1,32 @@
-const MyPage = () => {
-  return <div></div>;
+import styled from "styled-components";
+import { BackArrow } from "../../components/backArrow/BackArrow";
+
+const info = { id: "iddd", name: "정보운", studentId: "20222222" };
+
+export const MyPage = () => {
+  return (
+    <Container>
+      <Header>
+        <BackArrow />
+        <Title>마이페이지</Title>
+      </Header>
+
+      <Description>id: {info.id}</Description>
+      <Description>이름: {info.name}</Description>
+      <Description>학번: {info.studentId}</Description>
+    </Container>
+  );
 };
 
-export default MyPage;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Title = styled.p``;
+const Description = styled.p``;
