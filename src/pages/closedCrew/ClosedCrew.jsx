484d3bd1337d35000fd078a818d2c7f7380efc2b
Jeongbowoon <happyanne200212@gmail/com>
2025-02-09 02:02:21 +0900

    feat: closed-crew page 퍼블리싱

--- src/pages/closedCrew/ClosedCrew.jsx
+++ src/pages/closedCrew/ClosedCrew.jsx
@@ -3,19 +3,26 @@ import { CrewListItem } from "../../components/crewListItem/CrewListItem";
 import { CLOSED_CREW_LIST } from "../../../public/constants/constants";
 import styled from "styled-components";
 import { ToHomeButton } from "../../components/toHomeButton/ToHomeButton";
+import MyPageButton from "../../components/mypage/MyPageButton";
 
 export const ClosedCrew = () => {
   return (
     <Container>
-      <ToHomeButton />
-      {CLOSED_CREW_LIST.map((crew) => (
-        <CrewListItem
-          id={crew.id}
-          title={crew.title}
-          category={crew.category}
-          content={crew.content}
-        />
-      ))}
+      <Header>
+        <ToHomeButton />
+        <MyPageButton />
+      </Header>
+      <h1>기록열람</h1>
+      <ListContainer>
+        {CLOSED_CREW_LIST.map((crew) => (
+          <CrewListItem
+            id={crew.id}
+            title={crew.title}
+            category={crew.category}
+            content={crew.content}
+          />
+        ))}
+      </ListContainer>
     </Container>
   );
 };
@@ -24,7 +31,21 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 
+  padding: 30px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   gap: 10px;
 
-  padding: 20px;
+  height: 650px;
+  overflow-y: auto;
+
+  border-radius: 20px;
 `;
