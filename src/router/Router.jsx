70d83cff725fed263fbe4378886f3e2b62947019
Jeongbowoon <happyanne200212@gmail/com>
2025-02-08 21:34:10 +0900

    feat: 라우팅 설정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import { Onboarding } from "../pages/onboarding/Onboarding";
+import { Home } from "../pages/home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "", element: <Onboarding /> }],
+    children: [
+      { path: "", element: <Onboarding /> },
+      { path: "home", element: <Home /> },
+    ],
   },
 ]);
 
